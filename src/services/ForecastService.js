053eda7beaca6267600ea0d2332f2b74a429db35
Marre89 <git config --global user.email>
2021-03-21 16:55:29 +0100

    service

---
+++ src/services/ForecastService.js
@@ -0,0 +1,27 @@
+class ForecastService {
+    async getForecast() {
+        const BASE_URL = "https://opendata-download-metfcst.smhi.se"
+        const url = `${BASE_URL}/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json`
+        try {
+            const response = await fetch(url)
+            const temp = await response.json()
+            return temp
+        } catch (Error) {
+            return "OOps! Something went wrong when fetching the API"
+        }
+    }
+}
+
+const forecast = new ForecastService()
+
+async function forecastData() {
+    let weatherData = await forecast.getForecast()
+    if (weatherData != null) {
+        console.log(weatherData)
+    }
+
+    return weatherData
+}
+
+const _forecastData = forecastData()
+export { _forecastData as forecastData }
