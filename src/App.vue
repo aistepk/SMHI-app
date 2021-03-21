d6d56b69866fb344ad3608de46dcb306c03c8633
Marre89 <git config --global user.email>
2021-03-21 16:47:32 +0100

    Changed App and Navbar"

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-    <Navbar />
+    <Header />
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue"
+import Header from "./components/Header.vue"
 
 export default {
     name: "App",
     components: {
-        Navbar,
+        Header,
     },
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,29 +1,57 @@
 <template>
-    <div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-dark p-4">
-                <h5 class="text-white h4">Collapsed content</h5>
-                <span class="text-muted">Toggleable via the navbar brand.</span>
-            </div>
-        </div>
-        <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-light bg-light" aria-label="navigation">
+        <div class="container-fluid">
             <button
-                class="navbar-toggler"
+                class="navbar-toggler collapsed"
                 type="button"
-                data-toggle="collapse"
-                data-target="#navbarToggleExternalContent"
-                aria-controls="navbarToggleExternalContent"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarMain"
+                aria-controls="navbarMain"
                 aria-expanded="false"
                 aria-label="Toggle navigation"
             >
                 <span class="navbar-toggler-icon"></span>
             </button>
-        </nav>
-    </div>
+            <div class="d-flex justify-content-start">
+                <div class="navbar-collapse collapse" id="navbarMain">
+                    <div class="navbar-nav me-auto mb-2">
+                        <h6>Vädret just nu</h6>
+                        <div class="btn-group-vertical button-light" role="group" aria-label="Weather links">
+                            <span class="border-0">
+                                <button type="button" class="btn btn-light">Moln</button>
+                                <button type="button" class="btn btn-light">Vind</button>
+                                <button type="button" class="btn btn-light">Regn</button>
+                                <button type="button" class="btn btn-light">Om oss</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
     name: "Navbar",
+    data() {},
+    methods: {},
 }
 </script>
+
+<style scoped>
+.btn-group-vertical button {
+    text-align: left;
+    border: none;
+    padding: 1em;
+    font-size: large;
+}
+
+.btn-group-vertical:hover {
+    color: white;
+}
+
+.navbar-nav h6 {
+    padding-top: 1em;
+}
+</style>
