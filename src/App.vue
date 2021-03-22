8ab076b436b90a964cda8d3639d0ba1c5c70480a
Marre89 <git config --global user.email>
2021-03-22 23:30:52 +0100

    Added DropDown, fixed Cards component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
     <Header />
+    <drop-down-cities />
     <router-view />
 </template>
 
 <script>
+import DropDownCities from "./components/DropDownCities.vue"
 import Header from "./components/Header.vue"
 
 export default {
     name: "App",
-    components: { Header },
+    components: { Header, DropDownCities },
 }
 </script>
 
---
+++ src/components/DropDownCities.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container bg-light" style="height: 75px">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col mt-3">
+                    <div class="dropdown">
+                        <button
+                            class="btn btn-secondary dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuButton"
+                            data-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            Stad
+                        </button>
+                        <div class="dropdown-menu pre-scrollable" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Dingles</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "DropDown",
+    data() {
+        return {
+            cities: [
+                { name: "Göteborg", lng: 16.158, lat: 58.5812 },
+                { name: "Helsingborg", lng: 12.68, lat: 56.04 },
+                { name: "Jönköping", lng: 14.17, lat: 57.78 },
+                { name: "Linköping", lng: 15.67, lat: 58.38 },
+                { name: "Malmö", lng: 13.00073, lat: 55.60587 },
+                { name: "Stockholm", lng: 18.0649, lat: 59.33258 },
+                { name: "Uppsala", lng: 17.7, lat: 59.87 },
+                { name: "Västerås", lng: 16.55, lat: 59.6 },
+                { name: "Örebro", lng: 15.18, lat: 59.28 },
+            ],
+        }
+    },
+    methods() {},
+}
+</script>
