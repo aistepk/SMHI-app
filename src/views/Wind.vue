f6daa1a87e91fe02f1ed9a7b47f7e149e45e6c71
ShaSkol <shaism0914@skola.goteborg.se>
2021-04-06 12:22:24 +0200

    pushing wind-interface

--- src/views/Wind.vue
+++ src/views/Wind.vue
@@ -1,62 +1,67 @@
 <template>
-    <div id="header">
+    <!-- <div id="header">
         <h5>{{ wind }}m/s</h5>
-    </div>
+    </div> -->
 
-    <div id="container">
-        <div v-if="wind >= 0 && wind <= 3.9">
-            <h5>Svag vind</h5>
-            <img src="../assets/lightWind.gif" />
+    <div class="d-flex flex-column mb-3">
+        <div>
+            <p>{{ wind }}m/s</p>
+            <p>{{ temperate }}</p>
         </div>
-        <div v-if="wind >= 4 && wind <= 7.9">
-            <h5>Måttlig vind</h5>
-            <img src="../assets/lightWind.gif" />
+        <div>
+            <div v-if="wind >= 0 && wind <= 3.9">
+                <p>Svag vind</p>
+                <img src="../assets/breeze.gif" />
+            </div>
+            <div v-if="wind >= 4 && wind <= 7.9">
+                <p>Måttlig vind</p>
+                <img src="../assets/lightWind.gif" />
+            </div>
+            <div v-if="wind >= 8 && wind <= 13.8">
+                <p>Frisk vind</p>
+                <img src="../assets/windy.gif" />
+            </div>
+            <div v-if="wind >= 13.9 && wind <= 24.4">
+                <p>Hård vind</p>
+                <img src="../assets/windy.gif" />
+            </div>
+            <div v-if="wind >= 24.5 && wind <= 32.6">
+                <p>Storm</p>
+                <img src="../assets/storm.gif" />
+            </div>
+            <div v-if="wind >= 32.7">
+                <p>Orkan</p>
+                <img src="../assets/storm.gif" />
+            </div>
         </div>
-        <div v-if="wind >= 8 && wind <= 13.8">
-            <h5>Frisk vind</h5>
-            <img src="../assets/windy.gif" />
+        <div class="table-responsive mt-4">
+            <h6>Vindstyrka</h6>
+            <p>Här finns information om vad vindstyrka innebär</p>
+            <table class="table table-bordered border border-3">
+                <thead>
+                    <tr class="border border-4">
+                        <th scope="col" class="table-active">m/s</th>
+                        <th scope="col" class="border border-4">0-3,9</th>
+                        <th scope="col" class="border border-4">4-7,9</th>
+                        <th scope="col" class="border border-4">8-13,8</th>
+                        <th scope="col" class="border border-4">13,9-24,4</th>
+                        <th scope="col" class="border border-4">24,5-32,6</th>
+                        <th scope="col" class="border border-4">32,7-</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border border-4">
+                        <th scope="row" class="table-active">Styrka</th>
+                        <td class="border border-4">Svag</td>
+                        <td class="border border-4">Måttlig</td>
+                        <td class="border border-4">Frisk</td>
+                        <td class="border border-4">Hård</td>
+                        <td class="border border-4">Storm</td>
+                        <td class="border border-4">Orkan</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <div v-if="wind >= 13.9 && wind <= 24.4">
-            <h5>Hård vind</h5>
-            <img src="../assets/windy.gif" />
-        </div>
-        <div v-if="wind >= 24.5 && wind <= 32.6">
-            <h5>Storm</h5>
-            <img src="../assets/storm.gif" />
-        </div>
-        <div v-if="wind >= 32.7">
-            <h5>Orkan</h5>
-            <img src="../assets/windy.gif" />
-        </div>
-    </div>
-
-    <div id="footer">
-        <h6>Vindstyrka</h6>
-        <p>Här finns information om vad vindstyrka innebär</p>
-        <table class="table table-bordered border border-3" id="table">
-            <thead>
-                <tr class="border border-4">
-                    <th scope="col" class="table-active">m/s</th>
-                    <th scope="col" class="border border-4">0-3,9</th>
-                    <th scope="col" class="border border-4">4-7,9</th>
-                    <th scope="col" class="border border-4">8-13,8</th>
-                    <th scope="col" class="border border-4">13,9-24,4</th>
-                    <th scope="col" class="border border-4">24,5-32,6</th>
-                    <th scope="col" class="border border-4">32,7-</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="border border-4">
-                    <th scope="row" class="table-active">Styrka</th>
-                    <td class="border border-4">Svag</td>
-                    <td class="border border-4">Måttlig</td>
-                    <td class="border border-4">Frisk</td>
-                    <td class="border border-4">Hård</td>
-                    <td class="border border-4">Storm</td>
-                    <td class="border border-4">Orkan</td>
-                </tr>
-            </tbody>
-        </table>
     </div>
 </template>
 
@@ -67,39 +72,29 @@ export default {
     data() {
         return {
             wind: {},
+            temperate: {},
         }
     },
     async created() {
         this.wind = await findWindSpeed(16.158, 58.5812)
     },
+    methods: {
+        getTemperate() {
+            if (this.wind >= 4 && this.wind <= 7.9) {
+                this.temperate = this.wind
+            }
+        },
+    },
 }
 </script>
 <style lang="scss">
-#container > div h5 {
-    text-align: center;
-}
-#container > div img {
-    height: 300px;
+img {
     width: 100%;
     padding: 15px;
     border: 1px solid black;
     border-radius: 8px;
 }
-
-#header {
+p {
     text-align: center;
-}
-
-#footer {
-    height: auto;
-    width: 100%;
-    padding: 15px;
-    border: 1px solid rgb(214, 210, 210);
-    position: fixed;
-    bottom: 0;
-}
-
-#table{
-    width: 50%;
 }
 </style>
